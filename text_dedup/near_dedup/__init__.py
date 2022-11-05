c41c9e5423fcb9dd7525154f4c68825826407920
Chenghao Mou <mouchenghao@gmail.com>
2022-11-05 12:55:07 -0700

    redesign scripts

--- text_dedup/near_dedup/__init__.py
+++
@@ -1,5 +0,0 @@
-from text_dedup.near_dedup.minhash import MinHashEmbedder
-from text_dedup.near_dedup.simhash import SimHashEmbedder
-from text_dedup.near_dedup.simhash import SimHashIndex
-
-__all__ = ['SimHashEmbedder', 'SimHashIndex', 'MinHashEmbedder']
