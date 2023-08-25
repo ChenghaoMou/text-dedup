4c17d3007a43670ce294902bbfc8ca066cedc004
Chris Ha <hac541309@gmail.com>
2023-08-25 16:58:54 +0900

    docs and misc

--- text_dedup/minhash.py
+++ text_dedup/minhash.py
@@ -34,8 +34,6 @@ from text_dedup.utils.add_args import add_meta_args
 from text_dedup.utils.add_args import add_minhash_args
 from text_dedup.utils.analysis import optimal_param
 from text_dedup.utils.hashfunc import sha1_hash
-from text_dedup.utils.hashfunc import xxh3_16hash
-from text_dedup.utils.hashfunc import xxh3_32hash
 from text_dedup.utils.hashfunc import xxh3_hash
 from text_dedup.utils.timer import Timer
 
@@ -53,8 +51,6 @@ def embed_func(
     ngram_size: int,
     min_length: int,
     hashranges: List[Tuple[int, int]],
-    permutations: np.ndarray,
-    hash_func: Callable,
     dtype: type,
     max_hash: np.uint,
     modulo_prime: np.uint,
@@ -95,41 +91,28 @@ def embed_func(
     >>> hashranges = [(i, i + 25) for i in range(0, 250, 25)]
     >>> max_hash = np.uint32((1 << 32) - 1)
     >>> modulo_prime = np.uint32((1 << 32) - 5)
-    >>> PERMUTATIONS = np.array(
-    ...     [
-    ...         (
-    ...             RNG.randint(1, np.uint32((1 << 32) - 5), dtype=np.uint32),
-    ...             RNG.randint(0, np.uint32((1 << 32) - 5), dtype=np.uint32),
-    ...         )
-    ...         for _ in range(num_perm)
-    ...     ],
-    ...     dtype=np.uint32,
-    ... ).T
     >>> res = embed_func(content, idx, num_perm=num_perm, ngram_size=ngram_size, min_length=0, hashranges=hashranges,
-    ... permutations=PERMUTATIONS, hash_func=xxh3_32hash,dtype=np.uint32, max_hash=max_hash, modulo_prime=modulo_prime)
+    ... hash_func=xxh3_32hash,dtype=np.uint32, max_hash=max_hash, modulo_prime=modulo_prime)
     >>> len(res["__signatures__"])
     10
     >>> res["__id__"]
     0
     """
-    # a, b are each np.ndarray arrays containing {num_perm} pairs of random numbers used for building new hashes
-    # the formula is a * x(base hash of each shingle) + b
-    a, b = permutations
     # split content on whitespace (NON_ALPHA regex), tokenize with ngrams(), and join these n-grams into a single space separated string.
     # we then convert to lower case and then bytestrings which is then hashed. Only unique hashed n-grams are left.
     tokens: Set[bytes] = {
         bytes(" ".join(t).lower(), "utf-8") for t in ngrams(NON_ALPHA.split(content), ngram_size, min_length)
     }
 
-    hashes_1: np.ndarray = np.array([xxh3_hash(token, HASH_BITS) for token in tokens], dtype=dtype, ndmin=2).T
-    hashes_2: np.ndarray = np.array([sha1_hash(token, HASH_BITS) for token in tokens], dtype=dtype, ndmin=2).T
-    i: np.ndarray = np.arange(num_perm, dtype=dtype).reshape((1, num_perm))
+    hashes_1: np.ndarray = np.array([sha1_hash(token, HASH_BITS) for token in tokens], dtype=dtype, ndmin=2).T
+    hashes_2: np.ndarray = np.array([xxh3_hash(token, HASH_BITS) for token in tokens], dtype=dtype, ndmin=2).T
+    index: np.ndarray = np.arange(num_perm, dtype=dtype).reshape((1, num_perm))
     # Permute the hash values to produce new universal hashes
-    hashvalues = np.mod(hashes_1 + i * hashes_2 + i**2, modulo_prime, dtype=dtype)
+    hashvalues = (hashes_1 + index * hashes_2 + index**2) % modulo_prime
 
     # this part is where the name "min" of minhash comes from
     # this stacks all the hashes and then takes the minimum from each column
-    masks: np.ndarray = np.full(shape=num_perm, dtype=dtype, fill_value=max_hash)
+    masks: np.ndarray = np.full(shape=num_perm, fill_value=max_hash)
     hashvalues = np.vstack([hashvalues, masks]).min(axis=0)
     # Originally, byteswap was done for speed. Testing show it has a negligible impact
     # keeping  for backward compatibility, even though theoretically and empirically
@@ -156,7 +139,7 @@ if __name__ == "__main__":  # pragma: no cover
     # why legacy implementations used mersenne primes for modulo:
     # https://en.wikipedia.org/wiki/Universal_hashing#Hashing_strings
     HASH_CONFIG: Dict[int, Tuple[type, Any, Any]] = {
-        64: (np.uint64, np.uint64((1 << 32) - 1), np.uint64((1 << 61) - 1)),
+        64: (np.uint64, np.uint32((1 << 32) - 1), np.uint64((1 << 61) - 1)),
         # 32, 16 bit config does not use a mersenne prime.
         # The original reason for using mersenne prime was speed.
         # Testing reveals, there is no benefit to using a 2^61 mersenne prime for division
@@ -166,18 +149,6 @@ if __name__ == "__main__":  # pragma: no cover
 
     # defaults to backwards compatible HASH_BITS = 64, which is np.uint64 dtypes with 32bit hashes
     DTYPE, MAX_HASH, MODULO_PRIME = HASH_CONFIG.get(HASH_BITS, HASH_CONFIG[64])
-
-    match args.hash_func:
-        case "sha1":
-
-            def hash_func(byte_data):
-                return sha1_hash(byte_data, d=min(HASH_BITS, 32))
-
-        case "xxh3":
-            if HASH_BITS == 16:
-                hash_func = xxh3_16hash
-            else:
-                hash_func = xxh3_32hash
 
     # for is originally used to reduce memory usage in MacOS but also ensures that the Union Find data structure
     # is not copied to child processes as long as it is not modified.
@@ -219,21 +190,8 @@ if __name__ == "__main__":  # pragma: no cover
 
         LEN_DATASET = len(ds)
         # for minhash, we need to make a lot of hashes(=num_perms).
-        # In many previous implementations, this is achieved through a method described in
-        # `Universal classes of hash functions` https://doi.org/10.1016/0022-0000(79)90044-8
-        # There we start with a know good hash x (=hash_func) and permutate it as the following:
-        # `new_hash = (a * x + b) mod prime mod max_hash` we need one a (!=0), b pair per new hash
-        # the following produces these a, b pairs
-        PERMUTATIONS: np.ndarray = np.array(
-            [
-                (
-                    RNG.randint(1, MODULO_PRIME, dtype=DTYPE),  # a is a multiplier so should not be 0
-                    RNG.randint(0, MODULO_PRIME, dtype=DTYPE),  # b
-                )
-                for _ in range(args.num_perm)
-            ],
-            dtype=DTYPE,
-        ).T
+        # In this implementation we are using enhanced doublehash
+        # https://en.wikipedia.org/wiki/Double_hashing
 
         with timer("MinHashing"):
             embedded = ds.map(
@@ -243,8 +201,6 @@ if __name__ == "__main__":  # pragma: no cover
                     "hashranges": HASH_RANGES,
                     "ngram_size": args.ngram,
                     "min_length": args.min_length,
-                    "permutations": PERMUTATIONS,
-                    "hash_func": hash_func,
                     "dtype": DTYPE,
                     "max_hash": MAX_HASH,
                     "modulo_prime": MODULO_PRIME,
