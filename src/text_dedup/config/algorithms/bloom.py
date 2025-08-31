9f2d2e166b865a6630a0f3e6f37a4c9b57763107
Chenghao Mou <mouchenghao@gmail.com>
2025-08-31 14:13:00 +0100

    add bloom filter

--- src/text_dedup/config/algorithms/bloom.py
+++ src/text_dedup/config/algorithms/bloom.py
@@ -2,8 +2,15 @@
 # pyright: reportExplicitAny=false
 from typing import Literal
 
+from rbloom import Bloom
+
 from text_dedup.config.algorithms.base import AlgorithmConfig
 
 
 class BloomFilterAlgorithmConfig(AlgorithmConfig):
     algo_name: Literal["bloomfilter"] = "bloomfilter"
+    max_elements: int
+    error_rate: float
+
+    def get_filter(self) -> Bloom:
+        return Bloom(self.max_elements, self.error_rate)
